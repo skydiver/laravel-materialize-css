a835f6f2a70dc5514f29bf39b3170a814b67bcb7
Martin <martin@flydev.com>
2015-12-03 00:34:15 -0300

    class renamed

--- src/MaterializeCSS.php
+++ src/MaterializeCSS.php
@@ -4,7 +4,7 @@
 
     use Illuminate\Support\Facades\Facade;
 
-    class MaterializeCSSFacade extends Facade {
+    class MaterializeCSS extends Facade {
 
         protected static function getFacadeAccessor() { return 'materialize-css'; }
 
